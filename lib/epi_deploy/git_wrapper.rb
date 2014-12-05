603a45faa03d23282925cebc6bf76748f734767b
Ant Nettleship <a.nettleship@epigenesys.co.uk>
2014-12-05 16:40:10 +0000

    fixed the helper thing

--- lib/epi_deploy/git_wrapper.rb
+++ lib/epi_deploy/git_wrapper.rb
@@ -3,66 +3,67 @@ require_relative './helpers'
 
 module EpiDeploy
   class GitWrapper
-      def on_master?
-        git.current_branch == "master"
+    include EpiDeploy::Helpers
+    def on_master?
+      git.current_branch == "master"
+    end
+    
+    def pending_changes?
+      git.status.changed.any?
+    end
+    
+    def pull
+      git.pull
+    end
+    
+    def commit(message)
+      git.commit_all message
+    end
+    
+    def push(branch, options = {force: false, tags: true})
+      git.push 'origin', branch, options
+    end
+    
+    def add(files = nil)
+      git.add files
+    end
+    
+    def short_commit_hash
+      git.log.first.sha[0..6]
+    end
+    
+    def tag(tag_name)
+      git.add_tag(tag_name, annotate: true, message: tag_name)
+    end
+    
+    def get_commit(git_reference)
+      if git_reference == :latest
+        print_failure_and_abort("There is no latest release. Create one, or specify a reference with --ref") if tag_list.empty?
+        git_reference = tag_list.first
       end
       
-      def pending_changes?
-        git.status.changed.any?
-      end
-      
-      def pull
-        git.pull
-      end
-      
-      def commit(message)
-        git.commit_all message
-      end
-      
-      def push(branch, options = {force: false, tags: true})
-        git.push 'origin', branch, options
-      end
-      
-      def add(files = nil)
-        git.add files
-      end
-      
-      def short_commit_hash
-        git.log.first.sha[0..6]
-      end
-      
-      def tag(tag_name)
-        git.add_tag(tag_name, annotate: true, message: tag_name)
-      end
-      
-      def get_commit(git_reference)
-        if git_reference == :latest
-          print_failure_and_abort("There is no latest release. Create one, or specify a reference with --ref") if tag_list.empty?
-          git_reference = tag_list.first
-        end
-        
-        git_object_type = git.lib.object_type(git_reference)
+      git_object_type = git.lib.object_type(git_reference)
 
-        case git_object_type
-          when 'tag'    then git.tag(git_reference)
-          when 'commit' then git.object(git_reference)
-          else nil
-        end
+      case git_object_type
+        when 'tag'    then git.tag(git_reference)
+        when 'commit' then git.object(git_reference)
+        else nil
       end
-      
-      def change_branch_commit(branch, commit)
-        Kernel.system "git branch -f #{branch} #{commit}"
-        self.push branch, force: true, tags: true
+    end
+    
+    def change_branch_commit(branch, commit)
+      Kernel.system "git branch -f #{branch} #{commit}"
+      self.push branch, force: true, tags: true
+    end
+    
+    def tag_list(options = {limit: 5})
+      @tag_list ||= `git for-each-ref --sort=taggerdate --format '%(tag)' refs/tags`.split.reverse
+    end
+ 
+    private
+      def git
+        @git ||= ::Git.open(Dir.pwd)
       end
-      
-      def tag_list(options = {limit: 5})
-        @tag_list ||= `git for-each-ref --sort=taggerdate --format '%(tag)' refs/tags`.split.reverse
-      end
-   
-      private
-        def git
-          @git ||= ::Git.open(Dir.pwd)
-        end
     
   end
 end
