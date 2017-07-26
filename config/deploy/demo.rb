c4c9880be052022c6796c5489ccf35525c5ae297
James Gregory <james.gregory@epigenesys.org.uk>
2017-07-26 17:09:04 +0100

    Updated to support tests for failing deployments

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,6 +1,2 @@
 # Placeholder capistrano env deploy file for testing purposes
-namespace :deploy do
-  task :migrations do
-    abort 'Testing'
-  end
-end
+boom
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,1 @@
 # Placeholder capistrano env deploy file for testing purposes
-namespace :deploy do
-  task :migrations do
-  end
-end
