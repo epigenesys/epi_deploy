9a36e08a6f86ade5106bade72cc0737952dc5da1
James Gregory-Monk <james.gregory@epigenesys.org.uk>
2021-01-13 12:04:21 +0000

    Updated Aruba and fixed failing tests

--- spec/features/deploy_spec.rb
+++ spec/features/deploy_spec.rb
@@ -1,32 +1,30 @@
 require 'spec_helper'
 require 'support/aruba_helper'
 
-# TODO: These tests are failing when using the aruba git setup
-# expected that command "git push" has exit status of "0", but has "128".
 describe "Deploy", type: :aruba do
-  
+
   it "errors if environment doesn't exist" do
     setup_aruba_and_git
     run_ed 'deploy invalidenvironment'
-    assert_exit_status(1)
+    expect(last_command_started).to have_exit_status(1)
     expect(all_output).to include("Environment 'invalidenvironment' does not exist")
   end
-  
+
   it "errors if no latest release" do
     setup_aruba_and_git
     run_ed 'deploy production'
-    assert_exit_status(1)
+    expect(last_command_started).to have_exit_status(1)
     expect(all_output).to include("There is no latest release. Create one, or specify a reference with --ref")
   end
-  
+
   it "deploys latest release" do
     setup_aruba_and_git
-    run_simple 'git tag -a example_tag -m "For testing"'  # Create a pretend release
-    run_simple 'git push'
+    run_command_and_stop 'git tag -a example_tag -m "For testing"'  # Create a pretend release
+    run_command_and_stop 'git push'
     double_cmd('bundle')
     run_ed 'deploy production -r example_tag'
     expect(all_output).to include('Deploying to production...')
-    assert_exit_status(0)
+    expect(last_command_started).to have_exit_status(0)
   end
-  
+
 end
