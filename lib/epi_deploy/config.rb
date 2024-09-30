d6a5d9e98c229368c3dc9a810cbc6756874e210a
William Lee <william.lee@epigenesys.org.uk>
2024-09-30 11:33:37 +0100

    Add deprecation warning to use_tags_for_deploy option

--- lib/epi_deploy/config.rb
+++ lib/epi_deploy/config.rb
@@ -6,6 +6,8 @@ module EpiDeploy
   end
 
   def self.use_tags_for_deploy=(use_tags_for_deploy)
+    warn 'This option is now deprecated and will no effect on deployment'
+    warn 'Please remove this option from config/epi_deploy.rb as this may be removed in future releases' 
     @@use_tags_for_deploy = use_tags_for_deploy
   end  
 end
