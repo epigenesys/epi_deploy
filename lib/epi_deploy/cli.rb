fdb8cbc6e237f8807fa4872f1d469f5d213a1f99
William Lee <william.lee@epigenesys.org.uk>
2024-10-03 09:39:36 +0100

    Extract file contents in executables to Cli class

---
+++ lib/epi_deploy/cli.rb
@@ -0,0 +1,54 @@
+require 'slop'
+
+require_relative 'command'
+
+module EpiDeploy
+  class Cli
+    def run!
+      load_config
+
+      opts = Slop.parse strict: true do
+
+        banner 'Usage: bundle exec epi_deploy <command>'
+    
+        command 'release' do
+          description 'Create a new release with optional deploy'
+    
+          on :d=, :deploy=, 'Deploy to specified environment(s)', argument: :optional, as: Array, delimiter: ':'
+    
+          run do |options, args|
+            command = EpiDeploy::Command.new(options, args)
+            command.release
+          end
+        end
+    
+        command 'deploy' do
+          description 'Deploys an existing release'
+    
+          on :r=, :ref=, 'Git reference to deploy', argument: :optional
+    
+          run do |options, args|
+            command = EpiDeploy::Command.new(options, args)
+            command.deploy
+          end
+        end
+    
+        run do |options, args|
+          Kernel.abort "\x1B[31mValid commands are 'release' and 'deploy'.\x1B[0m"
+        end
+    
+      end
+    rescue Slop::InvalidOptionError, Slop::InvalidArgumentError, RuntimeError => e
+      Kernel.abort "\x1B[31m#{e.message}\x1B[0m"
+    end
+
+    private
+
+      def load_config
+        config_path = File.join(Dir.pwd, 'config/epi_deploy.rb')
+        if File.exist?(config_path)
+          require config_path
+        end
+      end
+  end
+end
