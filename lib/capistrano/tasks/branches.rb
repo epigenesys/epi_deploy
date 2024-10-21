b19bdafbce814039074a00ce53e5f6d62c7ce28f
William Lee <william.lee@epigenesys.org.uk>
2024-10-21 09:32:06 +0100

    Do not define set_branch cap task if using default deployment behaviour

--- lib/capistrano/tasks/branches.rb
+++ lib/capistrano/tasks/branches.rb
@@ -1,21 +1,25 @@
-if File.exist?('config/initializers/version.rb')
-  require File.join(Dir.pwd, 'config/initializers/version')
+require_relative '../../epi_deploy/config'
+
+Dir["config/initializers/version.rb", "config/epi_deploy.rb"].each do |file|
+  require File.join(Dir.pwd, file)
 end
 
-namespace :epi_deploy do
-  task :set_branch do
-    branch = if ENV['BRANCH']
-      ENV['BRANCH']
-    elsif Object.const_defined?('LATEST_RELEASE_TAG')
-      LATEST_RELEASE_TAG
-    end
+if EpiDeploy.use_timestamped_deploy_tags
+  namespace :epi_deploy do
+    task :set_branch do
+      branch = if ENV['BRANCH']
+        ENV['BRANCH']
+      elsif Object.const_defined?('LATEST_RELEASE_TAG')
+        LATEST_RELEASE_TAG
+      end
 
-    if branch.nil?
-      raise 'Cannot determine commit to deploy as BRANCH environment variable is not set and LATEST_RELEASE_TAG constant in version.rb could not be found'
-    end
+      if branch.nil?
+        raise 'Cannot determine commit to deploy as BRANCH environment variable is not set and LATEST_RELEASE_TAG constant in version.rb could not be found'
+      end
 
-    set :branch, branch
+      set :branch, branch
+    end
   end
-end
 
-before 'deploy:starting', 'epi_deploy:set_branch'
+  before 'deploy:starting', 'epi_deploy:set_branch'
+end
