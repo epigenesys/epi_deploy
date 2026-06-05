7d5617d20ee1d1b098adc8402d216958e5816125
William Lee <william.lee@epigenesys.org.uk>
2026-06-05 17:11:46 +0100

    Add test for existing release behaviour when version.rb already exists

--- spec/features/release_spec.rb
+++ spec/features/release_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 require 'support/aruba_helper'
 
+require "git"
+
 describe "Release", :bundle, type: :aruba do
+  let(:git) { Git.open(aruba.config.home_directory) }
+  let(:short_commit_hash) { git.log[1].sha[0..6] }
+  let(:expected_version) {}
+  let(:expected_release_tag) { "#{Time.now.strftime("%Y%b%d-%H%M")}-#{short_commit_hash}-v#{expected_version}".downcase }
+
   context "given EpiDeploy.create_release_commit option is configured to true" do
     before do
       write_file "config/epi_deploy.rb", <<~RUBY
@@ -9,12 +16,34 @@ describe "Release", :bundle, type: :aruba do
       RUBY
     end
 
-    it "creates a new release" do
-      run_ed "release"
+    context "given no version.rb is found" do
+      let(:expected_version) { 1 }
 
-      expect(last_command_started).to have_exit_status(0)
-      expect(all_output).to include("Release 1 created with tag #{Time.now.year}")
-      expect(all_output).not_to include 'Capistrano deploying to production'
+      it "creates a new release with version number 1 and does not deploy" do
+        run_ed "release"
+
+        expect(last_command_started).to have_exit_status(0)
+        expect(all_output).to include("Release #{expected_version} created with tag #{expected_release_tag}")
+        expect(all_output).not_to include 'Capistrano deploying to production'
+      end
+    end
+
+    context "given an version.rb exists with APP_VERSION set to 5" do
+      let(:expected_version) { 6 }
+
+      before do
+        write_file "config/initializers/version.rb", <<~RUBY
+          APP_VERSION = '5'
+        RUBY
+      end
+
+      it "creates a new release with version number and does not deploy" do
+        run_ed "release"
+
+        expect(last_command_started).to have_exit_status(0)
+        expect(all_output).to include("Release #{expected_version} created with tag #{expected_release_tag}")
+        expect(all_output).not_to include 'Capistrano deploying to production'
+      end
     end
 
     it "deploys the release if the flag is supplied" do
