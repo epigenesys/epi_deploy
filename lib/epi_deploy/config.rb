3b7bb8d52d967f01bb6af451657cfa25c31a71d0
William Lee <william.lee@epigenesys.org.uk>
2026-06-10 09:31:34 +0100

    Fix using output helpers in EpiDeploy module

--- lib/epi_deploy/config.rb
+++ lib/epi_deploy/config.rb
@@ -2,7 +2,7 @@ require_relative "helpers"
 
 module EpiDeploy
 
-  include Helpers
+  extend Helpers
 
   @use_tags_for_deploy = false
   @use_timestamped_deploy_tags = false
@@ -12,11 +12,24 @@ module EpiDeploy
 
     attr_reader :use_tags_for_deploy
     attr_accessor :use_timestamped_deploy_tags
-    attr_accessor :create_release_commit
+    attr_reader :create_release_commit
 
     def use_tags_for_deploy=(use_tags_for_deploy)
       print_warning "[Deprecation Warning] The use_tags_for_deploy option is now obsolete. Remove this from your configuration."
       @use_tags_for_deploy = use_tags_for_deploy
+    end
+
+    def create_release_commit=(value)
+      if value
+        print_warning <<~EOF.chomp
+          [Deprecation Warning] The create_release_commit option should only be used
+          if it is currently necessary for your workflow.
+
+          Please migrate to releasing without a commit, as releasing with a commit
+          will be removed in a future version. Instructions can be found in the README.
+        EOF
+      end
+      @create_release_commit = value
     end
 
     def create_release_commit?
